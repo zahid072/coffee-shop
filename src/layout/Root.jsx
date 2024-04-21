f341b485a3cd99e5643bfe20b351356058875a6a
zahid072 <jahidhasan19368@gmail.com>
2024-04-21 21:00:07 +0600

    coffee shop client

---
+++ src/layout/Root.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Root = () => {
+  // const handleSignIn = (e) => {
+  //   e.preventDefault();
+  //   const form = e.target;
+  //   const email = form.email.value;
+  //   const name = form.name.value;
+  //   const user = { name, email };
+
+  //   console.log(user)
+
+  //   fetch("http://localhost:5000/users", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(user),
+  //   })
+  //   .then(res => res.json())
+  //   .then(data => {
+  //       console.log(data)
+  //       alert("success")
+  //   })
+  // };
+  return (
+    <>
+     
+    </>
+  );
+};
+
+export default Root;
