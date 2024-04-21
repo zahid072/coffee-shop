6a4dde2b41fbc791537187b5bdbfc2fd90b8bc5a
zahid072 <jahidhasan19368@gmail.com>
2024-04-21 21:46:16 +0600

    add post method

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,31 +1,66 @@
 import React from "react";
 
 const Root = () => {
-  // const handleSignIn = (e) => {
-  //   e.preventDefault();
-  //   const form = e.target;
-  //   const email = form.email.value;
-  //   const name = form.name.value;
-  //   const user = { name, email };
+  const handleAddCoffee = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const chef = form.chef.value;
+    const name = form.name.value;
+    const coffee = { name, chef };
 
-  //   console.log(user)
+    console.log(coffee)
 
-  //   fetch("http://localhost:5000/users", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(user),
-  //   })
-  //   .then(res => res.json())
-  //   .then(data => {
-  //       console.log(data)
-  //       alert("success")
-  //   })
-  // };
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(coffee),
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        alert("success")
+    })
+  };
   return (
     <>
-     
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content md:w-96">
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleAddCoffee} className="card-body">
+              <h1 className="text-3xl font-bold text-center">Add a coffee</h1>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="name"
+                  name="name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">chef</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter coffee chef"
+                  name="chef"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Add Coffee</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
